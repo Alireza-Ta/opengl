b243825d331356b292533f382ca96e10946c2bf0
Alireza-Ta <infosoftwareha@yahoo.com>
2019-10-21 11:45:02 +0330

    before 16.

--- main.go
+++ main.go
@@ -6,12 +6,22 @@ import (
 
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
+	"github.com/go-gl/mathgl/mgl32"
 )
 
 var (
-	VAO    uint32
-	VBO    uint32
-	shader uint32
+	vao          uint32
+	vbo          uint32
+	ibo          uint32
+	shader       uint32
+	uniformModel int32
+	uniformProjection int32
+	direction            = true
+	triOffset    float32 = 0.0
+	triMaxOffset float32 = 0.7
+	triIncrement float32 = 0.0005
+	previousTime float64
+	frameCount   = 0
 )
 
 var (
@@ -21,13 +31,21 @@ var (
 
 //	Vertex Shader
 //	gl_Position is final position and return
+// vCol = vcolor; after vshader finished it is passed to other
+// shader which is fragment shader and put it in vCol in fshader.
 var vShader = `
 #version 330
 
 layout (location = 0) in vec3 pos;
 
+out vec4 vCol;
+
+uniform mat4 model;
+uniform mat4 projection;
+
 void main() {
-	gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);
+	gl_Position = projection * model * vec4(pos, 1.0);
+	vCol = vec4(clamp(pos, 0.0, 1.0), 1.0);
 }
 ` + "\x00"
 
@@ -35,10 +53,12 @@ void main() {
 var fShader = `
 #version 330
 
+in vec4 vCol;
+
 out vec4 color;
 
 void main() {
-	color = vec4(1.0, 0.0, 0.0, 1.0);
+	color = vCol;
 }
 ` + "\x00"
 
@@ -86,30 +106,67 @@ func main() {
 	// Get buffer size information
 	bufferWidth, bufferHeight = window.GetFramebufferSize()
 
+	gl.Enable(gl.DEPTH_TEST)
+
 	//	Setup Viewport size
 	gl.Viewport(0, 0, int32(bufferWidth), int32(bufferHeight))
 
 	createTriangle()
 	compileShader()
 
+	projection := mgl32.Perspective(45.0, float32(bufferWidth/bufferHeight), 0.1, 100)
+
+	previousTime = glfw.GetTime()
+	var curangle float32
 	//	Loop until window closed
 	for !window.ShouldClose() {
+		displayFPS()
+		// Get and handle user input events
+		glfw.PollEvents()
+
+		if direction {
+			triOffset += triIncrement
+		} else {
+			triOffset -= triIncrement
+		}
+
+		if mgl32.Abs(triOffset) >= triMaxOffset {
+			direction = !direction
+		}
+
 		//	Clear window
 		gl.ClearColor(0, 0.5, 0.5, 1)
-		gl.Clear(gl.COLOR_BUFFER_BIT)
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
 		gl.UseProgram(shader)
+		var model mgl32.Mat4
+		
+		curangle += 0.01
+		if curangle >= 360 {
+			curangle = .01
+		}
 
-		gl.BindVertexArray(VAO)
-		gl.DrawArrays(gl.TRIANGLES, 0, 3)
+		model = mgl32.Translate3D(0, 0, -2.5)
+		model = model.Mul4(mgl32.HomogRotate3D(mgl32.DegToRad(curangle), mgl32.Vec3{0,1,0}))
+		model = model.Mul4(mgl32.Scale3D(.4, .4, 1))
+
+		gl.Uniform1f(uniformModel, float32(triOffset))
+		gl.UniformMatrix4fv(uniformModel, 1, false, &model[0])
+		gl.UniformMatrix4fv(uniformProjection, 1, false, &projection[0])
+
+		gl.BindVertexArray(vao)
+
+		gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo)
+		gl.DrawElements(gl.TRIANGLES, 12, gl.UNSIGNED_INT, gl.PtrOffset(0))
+		gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, 0)
+
 		// unbind array
 		gl.BindVertexArray(0)
 		// unbind shader
 		gl.UseProgram(0)
 		// Do OpenGL stuff.
 		window.SwapBuffers()
-		// Get and handle user input events
-		glfw.PollEvents()
+
 	}
 }
 
@@ -167,22 +224,37 @@ func compileShader() {
 		gl.GetProgramInfoLog(shader, logLen, nil, &eLog[0])
 		log.Fatalf("error validating program: %s\n", string(eLog))
 	}
+
+	uniformModel = gl.GetUniformLocation(shader, gl.Str("model"+"\x00"))
+	uniformProjection = gl.GetUniformLocation(shader, gl.Str("projection"+"\x00"))
 }
 
 func createTriangle() {
+	indices := []uint32{
+		0, 3, 1,	// side
+		1, 3, 2,	// other sode
+		2, 3, 0,	// face
+		0, 1, 2,	// pyramid's base
+	}
 	vertices := []float32{
 		-1.0, -1.0, 0.0,
+		0.0, -1.0, 1.0,
 		1.0, -1.0, 0.0,
 		0.0, 1.0, 0.0,
 	}
 	// Grab somewhere in graphic memory to put data in it and gpu
-	// gives us the id of the memory in VAO.
-	gl.GenVertexArrays(1, &VAO)
-	gl.BindVertexArray(VAO)
+	// gives us the id of the memory in vao.
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
 
-	gl.GenBuffers(1, &VBO)
-	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
-	gl.BufferData(gl.ARRAY_BUFFER, len(vertices) * 4, gl.Ptr(vertices), gl.STATIC_DRAW)
+	gl.GenBuffers(1, &ibo)
+	// also call it ebo
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(indices) * 4, gl.Ptr(indices), gl.STATIC_DRAW)
+
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
 	// 0 = layout in shader = location = 0
 	// 3 = xyz in vertices
 	// float = type of xyz data
@@ -193,8 +265,21 @@ func createTriangle() {
 	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, gl.PtrOffset(0))
 	// layout set for 0 and enable the first arg in former function
 	gl.EnableVertexAttribArray(0)
-	// Unbind VBO
+	// Unbind vbo
 	gl.BindBuffer(gl.ARRAY_BUFFER, 0)
-	// Unbind VAO
+	// Unbind vao
 	gl.BindVertexArray(0)
+	// Remeber should unbind the ibo/ebo after unbinding the vao.
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, 0)
+}
+
+// mics functions
+func displayFPS() {
+	currentTime := glfw.GetTime()
+	frameCount++
+	if currentTime-previousTime >= 1.0 {
+		log.Println(frameCount)
+		frameCount = 0
+		previousTime = currentTime
+	}
 }
--- notes.md
+++ notes.md
@@ -1,2 +1,13 @@
 # Multiply glBufferData size argument
 Multiply len(data) * 4 unless it doesnt work. 4 indicates bytes of data type e.g float or int.
+
+# glGen* vs glCreate* and the DSA
+The purpose of `direct state access` is not to remove object binding from your application completely (that would be the purpose of the various "bindless" extensions). The purpose of direct state access is to allow you to access the state of an object without having to bind it (ie: directly).
+
+Pre-DSA, you had to bind a buffer just to allocate storage, upload data, etc. With DSA functions, you don't have to. You pass the buffer directly to the function you use to manipulate its state.
+
+But to actually use buffers in a rendering process, you must still bind it to the context or attach it to some other object which itself will get bound to the context.
+
+# Use gl.PtrOffset for something like indices
+
+# Depth_test for not showing back side
